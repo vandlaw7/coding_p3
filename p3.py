6915e6cbec4398095710bc31942378137772dd1f
vandlaw7 <vandlaw@naver.com>
2021-07-02 08:27:26 +0900

    finished

--- p3.py
+++ p3.py
@@ -7,41 +7,17 @@ counts_dict = dict()
 for i in rates_int:
     counts_dict[i] = counts_dict.get(i, 0) + 1
 
-counts_dict_sorted = dict(sorted(counts_dict.items()))
-
-cd = counts_dict_sorted.copy()
+cd = dict(sorted(counts_dict.items()))
 
 total_buy_want = 0
 previous_set_last_key = 10000
-while True:
-    buy_want = 0
-    first_key = 0
-    first_key_bool = True
-    for key, value in cd.items():
-        if value > 0:
-            value -= 1
-            buy_want += 1
-
-            if first_key_bool:
-                first_key = key
-                first_key_bool = False
-
-            last_key = key
-        else:
-            del cd[key]
-
-    total_buy_want += buy_want - 1
-    if previous_set_last_key < first_key:
+while len(cd) != 0:
+    total_buy_want += (len(cd) - 1)
+    if previous_set_last_key < list(cd)[0]:
         total_buy_want += 1
 
-    if all(value == 1 for value in cd.values()):
-        total_buy_want += buy_want - 1
-        if previous_set_last_key < first_key:
-            total_buy_want += 1
-        break
+    previous_set_last_key = list(cd)[-1]
 
-print(total_buy_want)
+    cd = {key: value - 1 for key, value in cd.items() if value != 1}
 
-
-
-
+print('total_buy_want', total_buy_want)
